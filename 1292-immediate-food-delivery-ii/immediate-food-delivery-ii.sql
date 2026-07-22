0395d02f2429cdf204bc8c1537febd6e1071cf7f
Abubakar <chabubakar855@gmail.com>
2026-07-22 11:44:22 -0400

    Time: 610 ms (83.10%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -9,9 +9,5 @@ FROM Delivery) t
 WHERE rn = 1)
 
 SELECT
-    ROUND((SELECT
-        COUNT(*)
-    FROM first_order
-    WHERE order_date = customer_pref_delivery_date) /
-    COUNT(*) * 100, 2) immediate_percentage
+    ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) immediate_percentage
 FROM first_order
