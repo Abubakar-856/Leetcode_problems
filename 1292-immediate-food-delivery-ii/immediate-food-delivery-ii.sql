cb8de771f96271704c2bff4df3c29aedb30b0f6c
Abubakar <chabubakar855@gmail.com>
2026-07-22 11:34:31 -0400

    Time: 656 ms (51.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH first_order AS(
+SELECT *
+FROM(
+SELECT 
+    *,
+    ROW_NUMBER() OVER( PARTITION BY customer_id ORDER BY order_date) rn
+FROM Delivery) t
+WHERE rn = 1)
+
+SELECT
+    ROUND((SELECT
+        COUNT(*)
+    FROM first_order
+    WHERE order_date = customer_pref_delivery_date) /
+    COUNT(*) * 100, 2) immediate_percentage
+FROM first_order
